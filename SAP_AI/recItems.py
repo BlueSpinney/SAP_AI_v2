e72c66bd84addfdc1c12279112c2b0b4f168476f
BlueSpinney <99137204+BlueSpinney@users.noreply.github.com>
2022-09-28 22:00:59 +0200

    v1.1

---
+++ SAP_AI/recItems.py
@@ -0,0 +1,12 @@
+import cv2 as cv
+import numpy as np
+
+def return_optimal_item_usage(curentItems):
+    pass
+
+def get_current_items():
+    items = []
+
+
+
+    return_optimal_item_usage(items)
