8208b7b1760f6ff807a83693129f73279aa5ee24
BlueSpinney <99137204+BlueSpinney@users.noreply.github.com>
2022-10-05 20:12:42 +0200

    v2

--- SAP_AI/SAP.py
+++ SAP_AI/SAP.py
@@ -1,16 +1,18 @@
+
 from tkinter import *
+from turtle import down
 import pyautogui
 import cv2 as cv
 import numpy as np
 import time
 import valueRecogniton as VR
 import itemrec as IR
-from random import randint
+from random import randint, randrange
 
 lost = False
 
 #add all the other animals
-loclst = ["ant","ape","bever","bison","bull","cricket","deer","dolphine","doodoo","duck","fish","giraff","godfly","gorila","hog","horse","kangoroo","leopard","mosqito","parrot","phantasticGus","pufffish","rabbit","racoon","rat","rihno","scorpio","seaotter","shark","sheep","shrimp","snail","snake","spider","stink","untitledGoose","worm"]
+loclst = ["ant","ape","badger","bafallo","bever","bull","camel","crab","cricket","crockodile","deer","dog","dolphine","doodoo","duck","elephant","fish","flamingo","giraff","gose","hedgehob","hippo","horse","kangaroo","lobstar","mosquito","otter","parrot","peacock","penguin","pig","pufferfish","rabbit","rat","rooster","scorpio","seal","shark","sheep","snail","spider","squril","stinky","turkey","turtle","whale","worm"]
 cordlst = []
 dub = False
 hitman = []
@@ -19,11 +21,12 @@ name_cordinates_1 = [(375, 399),(942, 399),(1528, 399)]
 name_cordinates_2 = [(375, 691),(942, 691),(1528, 691)]
 
 class slot:
-    def __init__(self,cor):
+    def __init__(self,cor,index):
         self.cor = cor
         self.empty = True
         self.animal = None
         self.val = None
+        self.index = index
     
     def set(self,animal_posistion,animal_name,new_val):
         if self.empty == False:
@@ -50,7 +53,7 @@ class slot:
     
 
 oldloc = (0,0)
-slots = [slot((524,441)), slot((671, 441)),slot((818, 441)),slot((965, 441)),slot((1112, 441))]
+slots = [slot((524,441),0), slot((671, 441),1),slot((818, 441),2),slot((965, 441),3),slot((1112, 441),4)]
 
 # not in use yet
 class Item:
@@ -98,13 +101,55 @@ def look_for_duplicat(name):
             return [True,i]
     return [False,None]
 
+#time O(n) Space O(n)
+def find_best_option_for_animal_replacement(_pd,down):
+    save = {}
+    for d in down:
+        save[len(save)] = d
+    ava = _pd
+    slotsava = slots
+
+    swap_list = {}
+    for slot in slotsava:
+        if slot.val == None:
+            slot.val = -1
+        for i in range(len(ava)):
+            if slot.val < ava[i][1] + ava[i][2]:
+                if slot.index not in swap_list:
+                    swap_list[slot.index] = [ava[i]]
+                    ava.pop(i)
+                    break
+                else:
+                    if swap_list[slot.index][1] + swap_list[slot.index][2] < ava[i][1] + ava[i][2]:
+                        swap_list[slot.index] = [ava[i]]
+                        ava[i].pop(i)
+                        break
+        if slot.val == -1:
+            slot.val = None
+
+    return(swap_list,save)
+
 def drag_and_drop(upper : list,down: list):
     global chupper
     upper = check_if_empty(upper)
     down = check_if_empty(down)
     chupper = sorted(upper,key=lambda x: x[1] + x[2])
 
-    mid = chupper[int(((len(chupper) - 1) / 2) - 0.1)][2] + chupper[int(((len(chupper) - 1) / 2) - 0.1)][1]
+    mid = chupper[0][2] + chupper[0][1]
+
+    _pd = down
+
+    #not in use yet
+    recived = find_best_option_for_animal_replacement(_pd,down)
+    for tokens in recived[1].keys():
+        down.append(recived[1][tokens])
+    
+    down = sorted(down, key= lambda x : x[3][0])
+    down = down[::-1]
+    optimal_replacement = recived[0]
+    print('\n')
+    print(optimal_replacement)
+
     for i in range(len(down)):
         if down[i][1] + down[i][2] > mid or look_for_empty() == True:
             duplicatVal = look_for_duplicat(down[i][0])
