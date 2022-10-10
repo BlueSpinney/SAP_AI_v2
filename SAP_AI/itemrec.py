3c12a9da4a00c9dd35230c42fdba0f27e41d4d30
BlueSpinney <99137204+BlueSpinney@users.noreply.github.com>
2022-10-10 21:01:50 +0200

    v3

--- SAP_AI/itemrec.py
+++ SAP_AI/itemrec.py
@@ -4,8 +4,9 @@ import numpy as np
 
 items = [['apple',' + 2'],['chilli',' + 5'],['choclate','+ 4'],['garlac','* 1.5'],['honey',' + 3'],['meat','+ 4']]
 cordinate_list = {}
+
 def get_current_items(img_name,i):
-    print(img_name)
+
 
     merge_img = cv.imread("C:\\Users\\Jerome\\OneDrive\\Desktop\\SAP_AI\\items\\" + img_name,0)
     loc = None
@@ -19,18 +20,19 @@ def get_current_items(img_name,i):
     w, h = merge_img.shape[::-1]
 
     res = cv.matchTemplate(img_gray,merge_img,cv.TM_CCOEFF_NORMED)
-    threshold = 0.60
+    threshold = 0.50
 
     loc = np.where( res >= threshold)
 
-    #Make functional for multiple items of the same type
+    # Make functional for multiple items of the same type
     for pt in zip(*loc[::-1]):
         cordinate_list[img_name[0:len(img_name) - 4]] = [items[i][1]]
         cordinate_list[img_name[0:len(img_name) - 4]].append(pt)
-    print(cordinate_list)
 
     
 def main():
+    global raw_corlst
+    raw_corlst = []
     for i in range(len(items)):
         parameter_for_item_gathering = items[i][0] + ".png"
         print('\n')
